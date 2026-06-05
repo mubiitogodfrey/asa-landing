f3ee6f1ac4fd45ddcd01f40a0ddcb60370487d8c
Mubiito Godfrey <allsafeafrica@gmail.com>
2026-06-05 01:11:46 -0700

    Create index.ts

---
+++ src/components/sections/index.ts
@@ -0,0 +1,8 @@
+// Export all sections
+export { HeroSection } from './HeroSection';
+export { NavigationMenu } from './NavigationMenu';
+export { MediaCards } from './MediaCards';
+export { FeaturedContentPanel } from './FeaturedContent';
+export { ContentHubSection } from './ContentHubSection';
+export { PartnersSection } from './PartnersSection';
+export { FooterSection } from './FooterSection';
